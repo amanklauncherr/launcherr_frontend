6a935ddd830c7d90b5f6163c6475021e18a3afd5
Nitin Kumar <nitinkumar@Nitins-MacBook-Pro.local>
2024-07-04 23:42:07 +0530

    done

--- src/components/GigsCard/index.js
+++ src/components/GigsCard/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import styles from './gigscard.module.css'
 import VerifyBadge from '../Icons/VerifyBadge'
 
-const GigsCard = ({gigs_duration, gigs_about, gigs_description, company_name, isVerified}) => {
+const GigsCard = ({isActive, gigs_duration, gigs_about, gigs_description, company_name, isVerified}) => {
+  if (isActive === 0) {
+    return null;
+  }
+
   return (
     <>
-      <div className={styles["gigs-card-main-container"]}>
+      <div className={styles["gigs-card-main-container"]} id="gigs-card">
           <p className={styles["gigs_type"]}>Duration <span>{gigs_duration} hrs</span> </p>
           <p className={styles["company_name"]}>
           {company_name} {isVerified && <VerifyBadge />}
