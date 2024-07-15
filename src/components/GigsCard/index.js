27b24e787935dcd51a0e4a14f97ec03db0f2f3ea
Nitin Kumar <nitinkumar@Nitins-MacBook-Pro.local>
2024-07-15 14:45:40 +0530

    gigs card short description add

--- src/components/GigsCard/index.js
+++ src/components/GigsCard/index.js
@@ -6,18 +6,30 @@ import VerifyBadge from '../Icons/VerifyBadge';
 import toast from 'react-hot-toast';
 import Clock from '../Icons/Clock';
 import { getCookie } from 'cookies-next';
-import LocationIcon from '../Icons/LocationIcon';
 import LocationIconBlack from '../Icons/LocationIconBlack';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import GigBadge from '../Icons/GigBadge';
 
-const GigsCard = ({gigs_badge,company_image, gig_id ,gigs_location, gigs_title, isActive, gigs_duration, gigs_about, gigs_description, company_name, isVerified }) => {
+const GigsCard = ({
+  gigs_Shortdescription,
+  gigs_badge,
+  company_image,
+  gig_id,
+  gigs_location,
+  gigs_title,
+  isActive,
+  gigs_duration,
+  gigs_about,
+  gigs_description,
+  company_name,
+  isVerified,
+}) => {
   const [note, setNote] = useState('');
   const reduxToken = useSelector((state) => state?.token?.publicToken);
 
   let bearerToken = '';
   const cookiesToken = getCookie('auth_token');
-  
+
   if (cookiesToken) {
     bearerToken = cookiesToken;
   } else {
@@ -28,29 +40,10 @@ const GigsCard = ({gigs_badge,company_image, gig_id ,gigs_location, gigs_title, 
     return null;
   }
 
+  const router = useRouter();
+
   const handleClick = async () => {
-    // try {
-    //   const payload = {
-    //     gigID: gig_id,
-    //     note: note || 'Default note if note is empty',
-    //   };
-
-    //   const response = await axios.post(
-    //     'https://api.launcherr.co/api/addEnquiry',
-    //     payload,
-    //     {
-    //       headers: {
-    //         Authorization: `Bearer ${bearerToken}`,
-    //       },
-    //     }
-    //   );
-    //  console.log(response)
-    // } catch (error) {
-    //   // toast.error('Please login first');
-    //   // console.error(error?.response?.data?.message);
-    //   toast.error(error?.response?.data?.message)
-    // }
-
+    // Your handleClick logic here
   };
 
   const handleViewDetails = () => {
@@ -58,36 +51,37 @@ const GigsCard = ({gigs_badge,company_image, gig_id ,gigs_location, gigs_title, 
   };
 
   return (
-    <div className={styles['gigs-card-main-container']} id='gigs-card'>
+    <div className={styles['gigs-card-main-container']} id="gigs-card">
       <div className={styles['gigs-cardinner']}>
-         <div className={styles["gigs-Badge"]}>
-          {gigs_badge ? <GigBadge/> : null}
-         </div>
-          <div className={styles["gig-image"]}>
-           <img src={company_image || "/icons/defaultimage.svg"} />
-          </div>
-          <p className={styles['company_name']}>
-            {company_name} {isVerified ? <VerifyBadge /> : null}
-          </p>
+        <div className={styles['gigs-Badge']}>
+          {gigs_badge ? <GigBadge /> : null}
+        </div>
+
+        <div className={styles['gig-image']}>
+          <img src={company_image || '/icons/defaultimage.svg'} />
+        </div>
+        <p className={styles['company_name']}>
+          {company_name} {isVerified ? <VerifyBadge /> : null}
+        </p>
 
         <div className={styles['gigs-card-gigs_title']}>
           <p>{gigs_title}</p>
         </div>
-        <div className={styles["gig-info-section"]}>
-        <p className={styles['gigs_type']}>
+        <div className={styles['gig-info-section']}>
+          <p className={styles['gigs_type']}>
             <Clock />
             {gigs_duration} hrs
           </p>
-          <div className={styles["Gigslocation"]}>
-            <LocationIconBlack/>
-           <p>{gigs_location}</p>
-        </div>
+          <div className={styles['Gigslocation']}>
+            <LocationIconBlack />
+            <p>{gigs_location}</p>
+          </div>
         </div>
         <p className={styles['gigs_about']}>{gigs_about}</p>
-        <p className={styles['gigs_description']}>{gigs_description}</p>
+        <p className={styles['gigs_description']}>{gigs_Shortdescription}</p>
       </div>
       <div className={styles['gigs-card-btn-sep']}>
-        <button onClick={handleViewDetails} className='book-btn-primary'>
+        <button onClick={handleViewDetails} className="book-btn-primary">
           View Details
         </button>
       </div>
