28007908c94d145f49da641dff878283dad7e2c0
Nitin Kumar <nitinkumar@Nitins-MacBook-Pro.local>
2024-07-21 15:06:37 +0530

    booking form bug

--- src/components/BookingForm/index.js
+++ src/components/BookingForm/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import styles from './BookingForm.module.css';
+import styles from './bookingform.module.css';
 
 const BookingForm = () => {
   const [departureDate, setDepartureDate] = useState(null);
