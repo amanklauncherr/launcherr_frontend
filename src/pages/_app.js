75269136ea39380f20ca86389902422da2ab4987
Nitin Kumar <nitinkumar@Nitins-MacBook-Pro.local>
2024-08-17 00:28:44 +0530

    gtag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
 import { persistor, store } from '@/store/store'
 import '@/styles/globals.css'
 import Head from 'next/head'
+import Script from 'next/script'
 import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-
 
 export default function App({ Component, pageProps }) {
   return (
@@ -16,13 +16,33 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
         <link rel="icon" type="image/x-icon" href="/favicon.svg" />
         <link href="https://fonts.cdnfonts.com/css/avenir" rel="stylesheet" />
+      {/* Meta Pixel Code */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '374999835704930');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style={{ display: 'none' }}
+             src="https://www.facebook.com/tr?id=374999835704930&ev=PageView&noscript=1" 
+        />
+      </noscript>
+      {/* End Meta Pixel Code */}
       </Head>
+      
+
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <Toaster
-            position="top-right"
-            reverseOrder={false}
-          />
+          <Toaster position="top-right" reverseOrder={false} />
           <Component {...pageProps} />
         </PersistGate>
       </Provider>
